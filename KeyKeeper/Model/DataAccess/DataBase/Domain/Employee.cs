e79d127bdfce48afb6f058d67b661d0ef228a195
p.gruszka <[omitted]>
2020-12-23 13:50:37 +0100

    Chosen approach: Code-first
    List of code changes:
    *) Installed EF NuGet package.
    *) Defined initial domain and context classes:
        - Employee, RoomKey,
        - CompanyDbContext.

---
+++ KeyKeeper/Model/DataAccess/DataBase/Domain/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyKeeper.Model.DataAccess.DataBase.Domain
+{
+    public class Employee
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Position { get; private set; }
+        public string Department { get; private set; }
+
+        public ICollection<RoomKey> RecievedRoomKeys { get; private set; } = new List<RoomKey>();
+
+        private Employee() { }
+        public Employee (string name, string surname, string position, string department)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Position = position;
+            this.Department = department;
+        }
+    }
+}
---
+++ KeyKeeper/Model/DataAccess/DataBase/Domain/RoomKey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyKeeper.Model.DataAccess.DataBase.Domain
+{
+    public class RoomKey
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomKeyId { get; private set; }
+        public string RoomName { get; private set; }
+
+        [ForeignKey(name: "EmployeeId")]
+        public Employee AssignedEmployee { get; set; }
+
+        private RoomKey() { }
+        public RoomKey(string roomName)
+        {
+            this.RoomName = roomName;
+        }
+    }
+}
