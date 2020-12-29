47a241ca8cb6b6b7bd6b53f804e42664ed42ebba
p.gruszka <[omitted]>
2020-12-29 16:24:53 +0100

    POCO clases are refactored to meet the task requirements.

---
+++ KeyKeeper/Model/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyKeeper.Model
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [MaxLength(4), MinLength(4)]
+        [Index(IsUnique = true)]
+        public string Employee_Id { get; protected set; }
+        [Required]
+        public string Name { get; protected set; }
+        [Required]
+        public string Surname { get; protected set; }
+        [Required]
+        public string Position { get; protected set; }
+        [Required]
+        public string Department { get; protected set; }
+
+        public ICollection<RoomKey> RecievedRoomKeys { get; protected set; } = new List<RoomKey>();
+
+        private Employee() { }
+        public Employee (string employeeCode, string name, string surname, string position, string department)
+        {
+            this.Employee_Id = employeeCode;
+            this.Name = name;
+            this.Surname = surname;
+            this.Position = position;
+            this.Department = department;
+        }
+        public void UpdateEmployee(Employee updatedEmployee)
+        {
+            this.Name = updatedEmployee.Name;
+            this.Surname = updatedEmployee.Surname;
+            this.Position = updatedEmployee.Position;
+            this.Department = updatedEmployee.Department;
+        }
+        public override string ToString()
+        {
+            return $"\"{Employee_Id}\";\"{Name}\";\"{Surname}\";\"{Position}\";\"{Department}\"";
+        }
+    }
+}
---
+++ KeyKeeper/Model/RoomKey.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyKeeper.Model
+{
+    public class RoomKey
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [MaxLength(4), MinLength(4)]
+        [Index(IsUnique = true)]
+        public string RoomKey_Id { get; protected set; }
+        [Required]
+        public string RoomName { get; protected set; }
+
+        public string AssignedEmployee_Id { get; protected set; }
+
+        [ForeignKey(name: "AssignedEmployee_Id")]
+        public Employee AssignedEmployee { get; set; }
+
+        private RoomKey() { }
+        public RoomKey(string keyCode, string roomName)
+        {
+            this.RoomKey_Id = keyCode;
+            this.RoomName = roomName;
+        }
+        public void UpdateRecord(RoomKey updatedRoomKey)
+        {
+            this.RoomName = updatedRoomKey.RoomName;
+        }
+        public override string ToString()
+        {
+            return $"\"{RoomKey_Id}\";\"{RoomName}\"";
+        }
+    }
+}
