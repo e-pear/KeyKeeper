290ef19cc4fbd3c79113de5915a67b3d53b62dbf
p.gruszka <[omitted]>
2020-12-30 15:24:06 +0100

    Some minor general Changes. *) initial connection string setting manager object. *) order in model folders. *) ...

---
+++ KeyKeeper/Model/DataAccess/CompanyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KeyKeeper.Model.DataAccess
+{
+    /// <summary>
+    /// Main db context class.
+    /// </summary>
+    public class CompanyDbContext : DbContext
+    {
+        // Constructor:
+        public CompanyDbContext() : base("name = ContextOnLocalServer") 
+        {
+            //Database.SetInitializer<CompanyDbContext>(new DropCreateDatabaseAlways<CompanyDbContext>()); <- temporary
+        }
+        // Entities:
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<RoomKey> RoomKeys { get; set; }
+    }
+}
---
+++ KeyKeeper/Settings/ConnectionSetting.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+
+namespace KeyKeeper.Settings
+{
+    /// <summary>
+    /// Simple setting class to "play" with connection string.
+    /// </summary>
+    public static class ConnectionSetting
+    {
+        /// <summary>
+        /// Restores default conection string. 
+        /// </summary>
+        public static void RestoreDefaultConnectionString()
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            string defaultConnectionString = "Server=DESKTOP;Database=KeyKeeperDataBase;User ID = KeyKeeperClient; password = KeyKeeperPassword";
+
+            config.ConnectionStrings.ConnectionStrings["ContextOnLocalServer"].ConnectionString = defaultConnectionString;
+            config.ConnectionStrings.ConnectionStrings["ContextOnLocalServer"].ProviderName = "System.Data.SqlClient";
+
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+        /// <summary>
+        /// Replaces current connection string by one with given parameters.
+        /// </summary>
+        /// <param name="server">Database server name.</param>
+        /// <param name="database">Database name itself.</param>
+        /// <param name="userId">Client login/id.</param>
+        /// <param name="password">Client password.</param>
+        public static void SetNewConnectionString(string server, string database, string userId, string password)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            string defaultConnectionString = $"Server = {server};Database = {database};User ID = {userId}; password = {password}";
+
+            config.ConnectionStrings.ConnectionStrings["ContextOnLocalServer"].ConnectionString = defaultConnectionString;
+            config.ConnectionStrings.ConnectionStrings["ContextOnLocalServer"].ProviderName = "System.Data.SqlClient"; ;
+
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+    }
+}
