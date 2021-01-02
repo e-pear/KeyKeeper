2ef8ab7b9f4f6974061fdf8a5101ffe000d16c6e
p.gruszka <[omitted]>
2021-01-02 11:08:32 +0100

    Moved and initially defined Main Window.

--- KeyKeeper/View/MainWindow.xaml.cs
+++ KeyKeeper/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace KeyKeeper
+namespace KeyKeeper.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
